38b03680c29ee8af1d8306847a33598be645970a
tkyg <80114433+tkyg@users.noreply.github.com>
2022-04-04 11:53:36 -0400

    created glassType and cocktailInstructions

--- script.js
+++ script.js
@@ -21,4 +21,12 @@ const renderOneDrink = (cocktail) =>{
   // 1b. cocktail name - strDrink
   const drinkName = document.createElement('h2')
   drinkName.innerText = cocktail.strDrink
+
+  // 1c. cocktail glass - strGlass
+  const glassType = document.createElement('h3')
+  glassType.innerText = cocktail.strGlass
+
+  // 1d. cocktail instruction - strInstructions
+  const instructions = document.createElement('h4')
+  instructions.innerText = cocktail.strInstructions
 }
